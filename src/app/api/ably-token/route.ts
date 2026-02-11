f2a32ee427094d8b8d78fd3f85bc820b052bd92d
EricMan <youzai0427@gmail.com>
2026-02-11 19:11:09 +0800

    feat add token generate

--- src/app/api/ably-token/route.ts
+++ src/app/api/ably-token/route.ts
@@ -1,14 +1,15 @@
 import { createAblyTokenRequest } from "@/src/library/server/ably";
+import { TokenRequest } from "ably";
 
-interface requestBody {
-  playerName: string;
+export interface createTokenRequest {
+  uuid: string;
 }
 
 // create ably token request for temporary authentication
 export async function POST(request: Request) {
   const body = request.json();
-  const { playerName }: requestBody = await body;
-  const token = await createAblyTokenRequest(playerName);
+  const { uuid }: createTokenRequest = await body;
+  const token: TokenRequest = await createAblyTokenRequest({ uuid });
   return new Response(JSON.stringify(token), {
     headers: { "Content-Type": "application/json" },
   });
