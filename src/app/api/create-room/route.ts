28d269a2810c78022e4732f35ce8889a40170cb2
EricMan <youzai0427@gmail.com>
2026-02-11 19:14:10 +0800

    feat: added token verification to improved secure

--- src/app/api/create-room/route.ts
+++ src/app/api/create-room/route.ts
@@ -1,12 +1,23 @@
 import { getRoomById } from "@/src/library/server/database";
 import { generateUID } from "@/src/utils/uuid";
 import { Room } from "../../types/room";
+import { TokenRequest } from "ably";
+import { isTokenExpired } from "@/src/library/server/ably";
 
-//TODO implement playerToken and use it to identify room host
+export interface CreateRoomRequest {
+  token: TokenRequest;
+  uuid: string;
+}
 
 export async function POST(request: Request) {
-  // const body = await request.json();
-  // const { playerToken } = body;
+  const body = await request.json();
+  const { token, uuid }: CreateRoomRequest = body;
+  if (!token.mac)
+    return new Response("Player token is required", { status: 400 });
+
+  if (isTokenExpired(token))
+    return new Response("Player token is expired", { status: 400 });
+
   let id = generateUID();
   let room: Room | null = await getRoomById(id);
 
@@ -15,10 +26,13 @@ export async function POST(request: Request) {
     id = generateUID();
     room = await getRoomById(id);
   }
+
   // create new room
   const roomData: Room = {
     id: id,
     scores: {},
+    createdAt: new Date(),
+    hostId: uuid,
   };
   return new Response(JSON.stringify(roomData), { status: 201 });
 }
