ea3df8812e059f354c361b2531641dd40eb0e51c
EricMan <youzai0427@gmail.com>
2026-02-12 18:05:56 +0800

    removed unused import

--- src/hooks/useJoinDialog.tsx
+++ src/hooks/useJoinDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function useJoinDialog() {
   const [openJoinDialog, setOpenJoinDialog] = useState<boolean>(false);
