5be23498395b08b775fb61b3593251407064e984
EricMan <youzai0427@gmail.com>
2026-02-12 18:07:58 +0800

    feat add create room and add expire to room

--- src/library/server/database.ts
+++ src/library/server/database.ts
@@ -1,11 +1,10 @@
 //TODO implement database interactions
 import { createClient } from "redis";
 import dotenv from "dotenv";
-import { Room } from "@/src/app/types/room";
+import { Room } from "@/src/types/room";
 dotenv.config({ path: ".env" });
 
 const PASSWORD = process.env.DATABASE_PASSWORD;
-
 const client = createClient({
   username: "default",
   password: PASSWORD,
@@ -20,17 +19,37 @@ client.on("error", (err) => console.log("Redis Client Error", err));
 await client.connect();
 
 export const getRoomById = async (id: string) => {
-  const result = (await client.json.get(`room-${id}`)) as Room | null;
+  const key = `room-${id}`;
+  const result = (await client.json.get(key)) as Room | null;
+  if (result) {
+    await client.expire(key, 10800); // reset TTL to 5 min
+  }
   return result;
+};
+
+// Function to set a JSON key with TTL
+export const createRoom = async (room: Room) => {
+  const key: string = `room-${room.id}`;
+  // Convert JS object to JSON string
+  const jsonString = JSON.stringify(room);
+  // setup options
+  // Set key with 30 minutes expiry (3 hours)
+  await client.json.set(key, "$", jsonString);
+  await client.expire(key, 10800);
+  console.log(`Key "${key}" set with 3 hours expiry.`);
 };
 
 export const getAllRooms = async (): Promise<Room[]> => {
   const keys = await client.keys("room-*");
-  const rooms = Object.fromEntries(
-    await Promise.all(
-      keys.map(async (key) => [key, await client.json.get(key)]),
-    ),
+  if (!keys.length) return [];
+
+  const rooms = await Promise.all(
+    keys.map(async (key) => {
+      // get object at root (not array)
+      const room = await client.json.get(key);
+      return room as unknown as Room;
+    }),
   );
 
-  return Object.values(rooms);
+  return rooms;
 };
