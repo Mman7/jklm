fbf02421ecaed434d67f7c51670c002aa0964430
EricMan <youzai0427@gmail.com>
2026-02-12 18:09:13 +0800

    fix: request is not json

--- src/app/api/ably-token/route.ts
+++ src/app/api/ably-token/route.ts
@@ -2,15 +2,19 @@ import { createAblyTokenRequest } from "@/src/library/server/ably";
 import { TokenRequest } from "ably";
 
 export interface createTokenRequest {
-  uuid: string;
+  playerId: string;
 }
 
 // create ably token request for temporary authentication
 export async function POST(request: Request) {
-  const body = request.json();
-  const { uuid }: createTokenRequest = await body;
-  const token: TokenRequest = await createAblyTokenRequest({ uuid });
-  return new Response(JSON.stringify(token), {
-    headers: { "Content-Type": "application/json" },
-  });
+  const formData = await request.formData();
+  const playerId = formData.get("playerId") as string;
+
+  if (!playerId) {
+    return new Response("Missing playerId", { status: 400 });
+  }
+
+  const token: TokenRequest = await createAblyTokenRequest({ playerId });
+
+  return Response.json(token);
 }
