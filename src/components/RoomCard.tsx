8cd6a4751c1591181d60764d9c5fe97613308fff
EricMan <youzai0427@gmail.com>
2026-02-12 18:02:53 +0800

    feat: implement Room list

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -1,17 +1,14 @@
-import { Room } from "@/src/app/types/room";
-import useUserValid from "@/src/hooks/useUserValid";
+import { Room } from "@/src/types/room";
 import useNameDialog from "@/src/zustands/useNameDialogStore";
 import { useRouter } from "next/navigation";
-import useAuth from "../zustands/useAuthStore";
+import useUserValid from "../hooks/useUserValid";
 
 export default function RoomCard({ room }: { room: Room }) {
   const router = useRouter();
   const { setShowDialog } = useNameDialog();
   const { isUserValid } = useUserValid();
-  const { token } = useAuth();
 
   const handleJoinRoom = () => {
-    console.log(token);
     if (!isUserValid) {
       setShowDialog(true);
       return;
@@ -24,7 +21,7 @@ export default function RoomCard({ room }: { room: Room }) {
       onClick={handleJoinRoom}
       className="min-w-2/12 rounded-lg bg-amber-300 p-4 transition-all duration-200 hover:scale-105 hover:cursor-pointer hover:bg-amber-400"
     >
-      <h1>{room.hostId.split("-")[0]}'s Room</h1>
+      <h1>{room.hostId}'s Room</h1>
       <h1>{room.id}</h1>
     </div>
   );
--- src/components/RoomList.tsx
+++ src/components/RoomList.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Room } from "../app/types/room";
+import { Room } from "../types/room";
 import RoomCard from "./RoomCard";
+import { getAllRooms } from "../library/client/client";
 
 export default function RoomList() {
   const [roomList, setRoomList] = useState<Room[]>([]);
   useEffect(() => {
-    fetch("/api/getall-room")
-      .then((res) => res.json())
-      .then((data) => setRoomList(data));
+    getAllRooms().then((data) => setRoomList(data));
   }, []);
 
   return (
     <section className="h-full w-full bg-green-400 p-8 pt-4">
       <h2 className="mb-2 text-2xl font-semibold">Join Public Room</h2>
-      <div className="flex flex-wrap justify-around gap-6 md:justify-start">
+      <div className="flex flex-wrap justify-around gap-6 sm:justify-start">
         {roomList.length > 0 &&
           roomList.map((room: Room) => <RoomCard key={room.id} room={room} />)}
       </div>
