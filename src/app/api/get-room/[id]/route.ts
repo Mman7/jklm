3de9110dd3c72114fdc4b34d16eae6553a561b3c
EricMan <youzai0427@gmail.com>
2026-02-12 18:08:51 +0800

    removed unused params

--- src/app/api/get-room/[id]/route.ts
+++ src/app/api/get-room/[id]/route.ts
@@ -1,12 +1,8 @@
-import { Room } from "@/src/app/types/room";
+import { Room } from "@/src/types/room";
 import { getRoomById } from "@/src/library/server/database";
-import { NextRequest } from "next/server";
 
 // return room data by id
-export async function GET(
-  request: NextRequest,
-  context: { params: { id: string } },
-) {
+export async function GET(context: { params: { id: string } }) {
   // this await is important, do not remove it
   const { id } = await context.params;
   const roomData: Room | null = await getRoomById(id);
