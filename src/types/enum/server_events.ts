6609bc92d146f869b572b8a5f2137bcbba8a95a6
Eric Man <youzai0427@gmail.com>
2026-02-23 18:18:26 +0800

    removed todo comments

--- src/types/enum/server_events.ts
+++ src/types/enum/server_events.ts
@@ -1,10 +1,3 @@
-//TODO subscript to events
-//  channel.publish("events", {
-//     text,
-//     playerId,
-//     timestamp: Date.now(),
-//   });
-
 export enum ServerEvent {
   PlayerAnsweredCorrectly = "player_answered_correctly",
   GameStarted = "game_started",
