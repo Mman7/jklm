1e493d4fc040c1c16d652770e819eefe131f5ba9
Eric Man <youzai0427@gmail.com>
2026-03-02 02:11:15 +0800

    feat: sort players by score in PlayerListChat and update rendering logic

--- src/components/game/PlayerListChat.tsx
+++ src/components/game/PlayerListChat.tsx
@@ -7,11 +7,11 @@ import { Player } from "@/src/types/player";
 export default function PlayerListChat() {
   const { channel } = useRoom();
   const { players } = useRoomPlayers(channel);
-
+  const sortedPlayers = [...players].sort((a, b) => b.score - a.score);
   return (
     <section className="border-base-content/10 bg-base-100/30 hidden w-48 flex-1 flex-col gap-3 border-l p-4 backdrop-blur-xl lg:flex">
-      {players.map((p: Player, index) => (
-        <PlayerCard key={index} player={p} />
+      {sortedPlayers.map((p: Player) => (
+        <PlayerCard key={p.playerId} player={p} />
       ))}
     </section>
   );
